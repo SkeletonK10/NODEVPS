8674d61410000b7c03f66e5e146415fcfed49f38
skeletonk10 <poohbilly2@naver.com>
2023-07-05 17:03:57 +0900

    BOJ 11561

---
+++ BOJ/11561.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define FastIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+//////New Type Definition//////////////////////////////////
+
+typedef long long ll;
+typedef std::pair<int, int> pii;
+typedef std::tuple<int, int, int> tiii;
+typedef std::complex<double> cpx;
+
+///////////////////////////////////////////////////////////
+
+using namespace std;
+
+void solve() {
+  ll n;
+  cin >> n;
+  ll l = 1, r = 1e9;
+  while (l < r) {
+    ll m = ( l + r ) / 2;
+    if (m * ( m + 1 ) / 2 > n)
+      r = m;
+    else
+      l = m + 1;
+  }
+  cout << l - 1 << endl;
+}
+
+int main() {
+  FastIO;
+  int testcase = 1;
+  cin >> testcase;
+  for (int tc = 1;tc <= testcase;tc++) {
+    solve();
+  }
+}
