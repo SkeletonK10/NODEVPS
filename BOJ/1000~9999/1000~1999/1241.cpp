a9e3e8d6979ec9ec261c26ef6b4eec0376f22369
skeletonk10 <poohbilly2@naver.com>
2023-11-30 12:57:06 +0900

    Comment BOJ 1241

--- BOJ/1000~9999/1000~1999/1241.cpp
+++ BOJ/1000~9999/1000~1999/1241.cpp
@@ -7,20 +7,25 @@ using namespace std;
 void solve() {
   int n;
   cin >> n;
-  vector<int> v(n), mp(1000001);
+  vector<int> v(n), mp(1000001);  // mp: 제한이 10^6이므로 벡터로 처리 가능
   for (int& i : v) {
     cin >> i;
     mp[i]++;
   }
+
+  // 약수 찾기
   for (int& i : v) {
     int ans = 0;
     for (int j = 1; j * j <= i;j++) {
       if (i % j == 0) {
         ans += mp[j];
+
+        //제곱수만 거르기
         if (j * j != i)
           ans += mp[i / j];
       }
     }
+    // 중요! 이때까지 센 값에는 자기 자신도 포함되므로 -1이 필요함.
     cout << ans - 1 << endl;
   }
 }
