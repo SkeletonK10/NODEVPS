8ecbe579bebe7d318e0e1a30f9e7b16e29d822e7
skeletonk10 <poohbilly2@naver.com>
2025-09-12 17:04:12 +0900

    BOJ 24516

---
+++ BOJ/24516.cpp
@@ -0,0 +1,27 @@
+//////Custom Preprocessor Directives/////////////////////////////////////////////////////
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define endl '\n'
+#define FastIO std::ios::sync_with_stdio(false); cin.tie(0);
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+void solve() {
+  int N;
+  cin >> N;
+  for (int i = 0;i < N;i++)
+    cout << i * 2 + 1 << ' ';
+}
+
+int main() {
+  FastIO;
+  int testcase = 1;
+  //cin >> testcase;
+  for (int tc = 1;tc <= testcase;tc++) {
+    solve();
+  }
+}
