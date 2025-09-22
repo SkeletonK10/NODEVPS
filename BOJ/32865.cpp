220cda6e3be448e902e644cd548a72a7cfbf0131
skeletonk10 <poohbilly2@naver.com>
2025-09-22 13:56:36 +0900

    mod: BOJ 32865

--- BOJ/32865.cpp
+++ BOJ/32865.cpp
@@ -1,11 +1,13 @@
 //////New Type Definition////////////////////////////////////////////////////////////////
 
 #include <vector>
+typedef long long ll;
 typedef std::pair<int, int> pii;
 
 //////Custom Preprocessor Directives/////////////////////////////////////////////////////
 
 #include <iostream>
+#include <vector>
 #include <algorithm>
 #include <queue>
 #define endl '\n'
@@ -21,7 +23,7 @@ void solve() {
   priority_queue<pii, vector<pii>, greater<pii>> pq;
   queue<int> ready;
 
-  int sum = 0;
+  ll sum = 0;
   for (int i = 1;i <= N;i++) {
     int temp;
     cin >> temp;
@@ -32,12 +34,16 @@ void solve() {
       ready.push(i);
   }
 
-  vector<int> ans;
+  if (sum != N - 1) {
+    cout << -1;
+    return;
+  }
+
   for (int i = 0;i < 2 * N - 1;i++) {
     if (i % 2) {
       auto [cnt, idx] = pq.top();
       pq.pop();
-      ans.push_back(idx);
+      cout << idx << endl;
       cnt--;
       if (cnt)
         pq.push({ cnt, idx });
@@ -45,16 +51,10 @@ void solve() {
         ready.push(idx);
     }
     else {
-      if (ready.empty()) {
-        cout << -1;
-        return;
-      }
-      ans.push_back(ready.front());
+      cout << ready.front() << endl;
       ready.pop();
     }
   }
-  for (int i : ans)
-    cout << i << endl;
 }
 
 int main() {
